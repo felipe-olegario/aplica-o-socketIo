0237baeecaed2bf3878c805571728e36da6df313
felipe.olegario <felipe.olegario@bandtec.com.br>
2022-05-22 01:04:57 -0300

    adjust rote

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-app.use('/', (req, res) => {
+app.use('https://web-socket.netlify.app/', (req, res) => {
     res.render('index.html');
 });
 
